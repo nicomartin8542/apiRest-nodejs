817d474320e1f40a9d4e2684d0098d12871611ba
Nicolas <nicomartin854@gmail.com>
2020-10-02 00:28:28 -0300

    Variable Entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,8 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB =
-        "mongodb+srv://admin:y9xmgWWlY4nCyx1d@cluster0.pykzg.mongodb.net/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
